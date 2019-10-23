c95371aa6c44203bac0e65018ba8192e62618c4b
Damian Moore <damian@epixstudios.co.uk>
2019-10-23 19:33:43 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+   name='matomo_monorail',
+   version='0.1.1',
+   description='Improve Matomo analytics with server-side traffic using Django',
+   author='Damian Moore',
+   author_email='damian@epixstudios.co.uk',
+   packages=['matomo_monorail', 'matomo_monorail.management', 'matomo_monorail.management.commands', 'matomo_monorail.migrations'],
+   install_requires=['django', 'requests'],
+)
